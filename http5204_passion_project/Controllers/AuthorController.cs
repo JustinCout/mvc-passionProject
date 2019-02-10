8959c544c344a02c107d36c5f55407ed26a61b13
JustinCoutinho <justincoutinho@live.ca>
2019-02-09 19:03:51 -0500

    good progress

--- http5204_passion_project/Controllers/AuthorController.cs
+++ http5204_passion_project/Controllers/AuthorController.cs
@@ -45,8 +45,7 @@ namespace http5204_passion_project.Controllers
                 
 
                 db.Database.ExecuteSqlCommand(query, param);
-                //testing that the paramters do indeed pass to the method
-                //Debug>Windows>Output
+         
                 Debug.WriteLine(query);
 
 
@@ -64,11 +63,8 @@ namespace http5204_passion_project.Controllers
         {
             string query = "select * from authors where authorid =@id";
 
-            //should return type of "Page"
-
             Debug.WriteLine(query);
-            //This line means run the query, take the first one
-
+            
             return View(db.Authors.SqlQuery(query, new SqlParameter("@id", id)).FirstOrDefault());
         }
 
@@ -83,7 +79,6 @@ namespace http5204_passion_project.Controllers
         [HttpPost]
         public ActionResult Edit(int? id, string AuthorAlias, string ImageType)
         {
-            //If the ID doesn't exist or the blog doesn't exist
             if ((id == null) || (db.Authors.Find(id) == null))
             {
                 return HttpNotFound();
@@ -94,55 +89,52 @@ namespace http5204_passion_project.Controllers
             param[0] = new SqlParameter("@name", AuthorAlias);
             param[1] = new SqlParameter("@image", ImageType);
             param[2] = new SqlParameter("@id", id);
-            //forcing the blog to have an author
-
-            db.Database.ExecuteSqlCommand(query, param);
-
             
+            db.Database.ExecuteSqlCommand(query, param);
 
             return RedirectToAction("Show/" + id);
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "AuthorID,AuthorAlias")] Author author, HttpPostedFileBase authorimg)
-        {
-            author.HasPic = 0;
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "AuthorID,AuthorAlias")] Author author, HttpPostedFileBase authorimg)
+        //{
+        //    author.HasPic = 0;
 
 
-            if (authorimg.ContentLength > 0)
-            {
-                //file extensioncheck taken from https://www.c-sharpcorner.com/article/file-upload-extension-validation-in-asp-net-mvc-and-javascript/
-                var valtypes = new[] { "jpeg", "jpg", "png", "gif" };
-                var extension = Path.GetExtension(authorimg.FileName).Substring(1);
+        //    if (authorimg.ContentLength > 0)
+        //    {
+        //        //file extensioncheck taken from https://www.c-sharpcorner.com/article/file-upload-extension-validation-in-asp-net-mvc-and-javascript/
+        //        var valtypes = new[] { "jpeg", "jpg", "png", "gif" };
+        //        var extension = Path.GetExtension(authorimg.FileName).Substring(1);
 
-                if (valtypes.Contains(extension))
-                {
+        //        if (valtypes.Contains(extension))
+        //        {
 
-                    //generic .img extension, web translates easily.
-                    string fn = author.AuthorId + "." + extension;
+        //            //generic .img extension, web translates easily.
+        //            string fn = author.AuthorId + "." + extension;
 
-                    //get a direct file path to imgs/authors/
-                    string path = Path.Combine(Server.MapPath("~/images/authors"), fn);
+        //            //get a direct file path to imgs/authors/
+        //            string path = Path.Combine(Server.MapPath("~/images/authors"), fn);
 
-                    //save the file
-                    authorimg.SaveAs(path);
+        //            //save the file
+        //            authorimg.SaveAs(path);
 
-                    //let the model know that there is a picture with an extension
-                    author.HasPic = 1;
-                    author.ImageType = extension;
+        //            //let the model know that there is a picture with an extension
+        //            author.HasPic = 1;
+        //            author.ImageType = extension;
 
-                }
-            }
-            if (ModelState.IsValid)
-            {
-                db.Entry(author).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Show");
-            }
+        //        }
+        //    }
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(author).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Show");
+        //    }
 
-            return View(author);
-        }
+        //    return View(author);
+        //}
 
         public ActionResult Delete(int? id)
         {
@@ -151,16 +143,11 @@ namespace http5204_passion_project.Controllers
                 return HttpNotFound();
 
             }
-            //These three statements should definitely be wrapped in
-            //a stored procedure instead of me manually doing it
-            //key term here "referential integrity"
-
             
             string query = "delete from Reviews where Authors_AuthorId=@id";
             SqlParameter param = new SqlParameter("@id", id);
             db.Database.ExecuteSqlCommand(query, param);
 
-            //finally delete blog
             query = "delete from Authors where AuthorId=@id";
             param = new SqlParameter("@id", id);
             db.Database.ExecuteSqlCommand(query, param);
